a003a95971e9475f8b104ec92afc57036cea221e
Janet Webster <twixmixy@gmail.com>
2023-11-02 14:28:43 -0400

    Routes done

--- routes/api/trip-routes.js
+++ routes/api/trip-routes.js
@@ -1,26 +1,95 @@
 const router = require('express').Router();
 const { Traveller, Location, Trip } = require('../../models');
 
-router.get('/', (req, res) => {
-    // find all categories
-    // be sure to include its associated Products
+router.get('/', async (req, res) => {
+    try {
+        const tripsAll = await Trip.findAll({
+            include: [{ model: Location }]
+        });
+        
+        if (!tripsAll) {
+            res.status(200).json({ message: "No trips found"});
+            return;
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
   });
   
-  router.get('/:id', (req, res) => {
-    // find one category by its `id` value
-    // be sure to include its associated Products
+  router.get('/:id', async (req, res) => {
+    try {
+        const tripID = await Trip.findByPk(req.params.id, {
+            include: [{ model: Location }]
+        });
+        
+        if (!tripID) {
+            res.status(200).json({ message: "Could not find that trip"});
+            return;
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
   });
   
-  router.post('/', (req, res) => {
-    // create a new category
+  router.post('/', async (req, res) => {
+    try {
+        const newTrip = await Trip.create({
+          name: req.body.name
+        });
+    
+        res.status(200).json(newTrip);
+        console.log('Added new trip!');
+    
+      } catch (err) {
+        res.status(500).json(err);
+      };
   });
   
-  router.put('/:id', (req, res) => {
-    // update a category by its `id` value
+  router.put('/:id', async (req, res) => {
+    try {
+        const updateTrip = await Trip.update({
+          name: req.body.name
+        },
+        {
+          where: {
+            id: req.params.id
+          }
+        });
+    
+        const tripID = await Trip.findByPk(req.params.id, {
+          include: [{ model: Location }]
+        });
+    
+        if (!tripID) {
+          res.status(200).json({ message: 'Could not find that trip' });
+          return;
+        };
+    
+        res.status(200).json(updateTrip);
+        console.log("Updated trip!")
+      } catch (err) {
+        res.status(500).json(err);
+      }
   });
   
-  router.delete('/:id', (req, res) => {
-    // delete a category by its `id` value
+  router.delete('/:id', async (req, res) => {
+    try {
+        const destroyTrip = await Trip.destroy({
+          where: {
+            id: req.params.id
+          }
+        });
+    
+        if (!destroyTrip) {
+          res.status(200).json({ message: 'Could not find that trip' });
+          return;
+        };
+    
+        res.status(200).json(destroyTrip);
+        console.log('This trip has been destroyed!')
+      } catch {
+        res.status(500).json(err);
+      };
   });
   
   module.exports = router;
