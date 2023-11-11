f887b94d56c5717506490f45a99b0e71ac3c2dda
Janet Webster <twixmixy@gmail.com>
2023-11-11 18:59:26 -0500

    Updated model connections

--- models/index.js
+++ models/index.js
@@ -2,26 +2,40 @@
 const Traveller = require('./Traveller');
 const Location = require('./Location');
 const Trip = require('./Trip');
-const LocationTrip = require('./LocationTrip');
+// const LocationTrip = require('./LocationTrip');
 
+Traveller.belongsToMany(Location, {
+  through: {
+    unique: false
+  },
+  as: 'planned_trips'
+});
 
-Location.belongsTo(Traveller, {
-  foreignKey: 'traveller_id'
- });
+Location.belongsToMany(Traveller, {
+  through: {
+    model: Trip,
+    unique: false
+  },
+  as: 'location_travellers'
+});
 
-Traveller.hasMany (Location, {
-  foreignKey: 'location_id'
- });
+// Location.belongsTo(Traveller, {
+//   foreignKey: 'traveller_id'
+//  });
+
+// Traveller.hasMany (Location, {
+//   foreignKey: 'location_id'
+//  });
  
-Location.belongsToMany(Trip,{
-  through: LocationTrip,
-  foreignKey: 'location_id',
-});
+// Location.belongsToMany(Trip,{
+//   through: LocationTrip,
+//   foreignKey: 'location_id',
+// });
 
-Trip.belongsToMany(Location,{
-  through: LocationTrip,
-  foreignKey: 'trip_id',
-});
+// Trip.belongsToMany(Location,{
+//   through: LocationTrip,
+//   foreignKey: 'trip_id',
+// });
 
 module.exports = {
   Traveller,
