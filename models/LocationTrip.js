78a0bb492c3ae9cc1daa398dbc30be6052d6e4a9
Janet Webster <twixmixy@gmail.com>
2023-11-11 19:03:56 -0500

    LocationTrip model not needed

--- models/LocationTrip.js
+++ models/LocationTrip.js
@@ -1,40 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-class LocationTrip extends Model {}
+// class LocationTrip extends Model {}
 
-LocationTrip.init (
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        location_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'Location',
-                key: 'id',
-            },
-        },
-        trip_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'Trip',
-                key: 'id',
-            },
-        },
-    },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'location_trip',
-      },
-    );
+// LocationTrip.init (
+//     {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             primaryKey: true,
+//             autoIncrement: true,
+//         },
+//         location_id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             references: {
+//                 model: 'Location',
+//                 key: 'id',
+//             },
+//         },
+//         trip_id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             references: {
+//                 model: 'Trip',
+//                 key: 'id',
+//             },
+//         },
+//     },
+//     {
+//         sequelize,
+//         timestamps: false,
+//         freezeTableName: true,
+//         underscored: true,
+//         modelName: 'location_trip',
+//       },
+//     );
 
-module.exports = LocationTrip;
+// module.exports = LocationTrip;
